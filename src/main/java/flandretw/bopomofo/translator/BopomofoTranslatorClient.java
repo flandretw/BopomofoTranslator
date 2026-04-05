600cae5df3b23fb74e04e1324e02e4b5f1492589
蘭蘭露 <51469621+flandretw@users.noreply.github.com>
2026-04-05 15:09:16 +0800

    Initial commit

---
+++ src/main/java/flandretw/bopomofo/translator/BopomofoTranslatorClient.java
@@ -0,0 +1,10 @@
+package flandretw.bopomofo.translator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BopomofoTranslatorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/flandretw/bopomofo/translator/BopomofoTranslatorDataGenerator.java
@@ -0,0 +1,11 @@
+package flandretw.bopomofo.translator;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BopomofoTranslatorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
